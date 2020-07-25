2819fe09c4a2a8bd7414e3e4f4ccc759e3bba464
Chris Walsh <attrition.h@gmail.com>
2020-07-25 14:56:28 -0400

    migrate to octavius.inf

--- src/widget/octavius_ui/city.c
+++ src/widget/octavius_ui/city.c
@@ -48,11 +48,6 @@ static struct {
     int first_focus;
 } data;
 
-void widget_octavius_ui_city_draw_background(void)
-{
-
-}
-
 static void enable_building_buttons(void)
 {
     for (int i = 0; i < 12; i++) {
@@ -127,6 +122,11 @@ void draw_minimap(void)
     int map_offset_y = screen_height() - 160 - map_grid_height();
     widget_minimap_invalidate();
     widget_minimap_draw(map_offset_x, map_offset_y, map_grid_width(), map_grid_height() * 2, 0);
+}
+
+void widget_octavius_ui_city_draw_background(void)
+{
+
 }
 
 void widget_octavius_ui_city_draw_foreground(void)
