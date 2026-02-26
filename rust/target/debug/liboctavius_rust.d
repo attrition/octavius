2f03c62f6364b2092c4c2abade4fad261e21b3cb
Chris Walsh <attrition.h@gmail.com>
2026-02-26 01:01:44 -0500

    chore: remove rust build artifacts from repo and update .gitignore

--- rust/target/debug/liboctavius_rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\astud\dev\octavius\rust\target\debug\liboctavius_rust.rlib: C:\Users\astud\dev\octavius\rust\src\calc.rs C:\Users\astud\dev\octavius\rust\src\lib.rs
--- rust/target/debug/octavius_rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\astud\dev\octavius\rust\target\debug\octavius_rust.lib: C:\Users\astud\dev\octavius\rust\src\calc.rs C:\Users\astud\dev\octavius\rust\src\lib.rs
